45bdf4247e9a93f516596962d67405bdf9eb66cb
Engels Mejia <75344303+Engels-Mejia@users.noreply.github.com>
2023-03-09 18:58:00 -0500

    Delete Secrets.cs

--- Data/Secrets.cs
+++
@@ -1,9 +0,0 @@
-namespace Data;
-
-internal class Secrets
-{
-
-    private const string _connectionString = "Server=tcp:230206net-p2-server.database.windows.net,1433;Initial Catalog=TeamA;Persist Security Info=False;User ID=teamA;Password=d@mag5C0ntrol!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
-    public static string getConnectionString() => _connectionString;
-}
