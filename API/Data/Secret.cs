bdaf4f33b50ac645d5a76e3fcae7a1bd5c579b6c
main <eperry910@yahoo.com>
2023-03-10 01:41:48 -0500

    Made baseline presentation

---
+++ API/Data/Secret.cs
@@ -0,0 +1,7 @@
+
+public class Secrets{
+    private static string connectionString = "Server=tcp:230206net-p2-server.database.windows.net,1433;Initial Catalog=TeamA;Persist Security Info=False;User ID=teamA;Password=d@mag5C0ntrol!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    public static string getConnectionString(){
+        return connectionString;
+    }
+}
--- API/Models/Ticket.cs
+++ API/Models/Ticket.cs
@@ -1,4 +1,42 @@
+﻿using System;
 namespace Models;
-public class Ticket{
-    int id;
+public class Ticket
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public int ClientId { get; set; }
+    public DateTime SubmissionDate { get; set; } = DateTime.Today;
+    public DateTime DamageDate { get; set; }
+    public string Description { get; set; }
+    public string DamagerId { get; set; }
+
+    public Ticket(int Id, decimal Amount, int ClientId, DateTime DamageDate, string Description, string DamagerId)
+    {
+        this.Id = Id;
+        this.Amount = Amount;
+        this.ClientId = ClientId;
+        this.DamageDate = DamageDate;
+        this.Description = Description;
+        this.DamagerId = DamagerId;
+    }
+    public Ticket(int Id, decimal Amount, int ClientId, DateTime SubmissionDate, DateTime DamageDate, string Description, string DamagerId)
+    {
+        this.Id = Id;
+        this.Amount = Amount;
+        this.ClientId = ClientId;
+        this.SubmissionDate = SubmissionDate;
+        this.DamageDate = DamageDate;
+        this.Description = Description;
+        this.DamagerId = DamagerId;
+    }
+    public Ticket(decimal Amount, string Description, string DamagerId)
+    {
+        this.Amount = Amount;
+        this.ClientId = 1;
+        this.SubmissionDate = DateTime.Today;
+        this.DamageDate = DateTime.Today;
+        this.Description = Description;
+        this.DamagerId = DamagerId;
+    }
+
 }
