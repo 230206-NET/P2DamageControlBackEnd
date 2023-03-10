913be4181693ed6a189d8e2aa42e677df3b36278
main <eperry910@yahoo.com>
2023-03-09 19:45:08 -0500

    Added ability to query Marvel API

--- API/wwwroot/js/searchForDamager.js
+++ API/wwwroot/js/searchForDamager.js
@@ -1,5 +1,5 @@
-
 async function retrieveDamagers() {
     const damageInfo = document.getElementById("Damager")
-    fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&nameStartsWith=' + damageInfo.value + '&apikey=8ecae25f58b2e8b15e9eaded61953912').then((res) => res.json()).then(data => console.log(data))
+    var hash = CryptoJS.MD5("16aed8bb2db92dc0d6f5e6ca7059b194ff52b92228ecae25f58b2e8b15e9eaded61953912").toString()
+    fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&nameStartsWith=' + damageInfo.value + '&apikey=8ecae25f58b2e8b15e9eaded61953912&hash=' + hash).then((res) => res.json()).then(data => console.log(data))
 }
