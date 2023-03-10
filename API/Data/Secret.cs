adbb9dc56b1801370dc466dd4f2d80a2d0c51950
main <eperry910@yahoo.com>
2023-03-10 15:43:05 -0500

    Final commit

--- API/Data/Secret.cs
+++
@@ -1,7 +0,0 @@
-
-public class Secrets{
-    private static string connectionString = "Server=tcp:230206net-p2-server.database.windows.net,1433;Initial Catalog=TeamA;Persist Security Info=False;User ID=teamA;Password=d@mag5C0ntrol!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    public static string getConnectionString(){
-        return connectionString;
-    }
-}
