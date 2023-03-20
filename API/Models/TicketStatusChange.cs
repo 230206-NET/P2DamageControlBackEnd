2b864baa2a71d5d3e49e17898f3f9de979077627
main <eperry910@yahoo.com>
2023-03-20 14:19:54 -0400

    Added accessLevel to TicketStatusChange

--- API/Models/TicketStatusChange.cs
+++ API/Models/TicketStatusChange.cs
@@ -1,3 +1,3 @@
 namespace Models;
 
-public record TicketStatusChange(int UserId, int Status, string Justification, int TicketId);
+public record TicketStatusChange(int UserId, int Status, string Justification, int TicketId, int AccessLevel);
